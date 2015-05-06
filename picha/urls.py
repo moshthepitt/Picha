264cba37decdaf20c87a69e84141f1b4a1d64b1f
moshthepitt <kelvin@jayanoris.com>
2015-05-06 16:20:24 +0300

    view and utils to add and display photos

--- picha/urls.py
+++ picha/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from photos.views import PhotoView
+
 urlpatterns = [
+    url(r'', PhotoView.as_view()),
     url(r'^admin/', include(admin.site.urls)),
 ]
