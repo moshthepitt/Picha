a3c0cf12bb3d404350451343d7a835a9913df6ca
moshthepitt <kelvin@jayanoris.com>
2015-05-06 17:12:15 +0300

    Adding redis for celry brokerage and fixed up things a little

--- picha/settings.py
+++ picha/settings.py
@@ -108,3 +108,11 @@ STATICFILES_DIRS = (
 )
 STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
+
+# CELERY
+BROKER_URL = 'redis://localhost:6379/8'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/8'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Africa/Nairobi'
